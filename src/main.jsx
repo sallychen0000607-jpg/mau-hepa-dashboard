195c432805bf45a85c818a9909e7e56fc5f4af7c
sallychen0000607-jpg <sallychen0000607@gmail.com>
2026-05-27 17:23:33 +0800

    src/index.css

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MAUHEPADashboard from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MAUHEPADashboard />
+  </React.StrictMode>
+);
